76d597875762684183c202355e0a567d39465392
Jesús Quiroz Rojas <142179535+JesusCK@users.noreply.github.com>
2024-04-26 10:31:15 -0500

    Add files via upload

--- GUI2.py
+++ GUI2.py
@@ -9,6 +9,7 @@ from threading import Thread
 from queue import Queue
 import time
 import imageio
+import requests
 
 class AppCamera:
     threshold = 0.5
@@ -19,10 +20,16 @@ class AppCamera:
                         'Levantandose',
                         'Sentado',
                         'Caminando'])
+    REMOTE_HOST = '18.220.249.217'
+# Puerto en el que se ejecuta el servidor Flask
+    REMOTE_PORT = 5000
+    # URL para la recepción de datos en el servidor Flask
+    RECEIVE_URL = f"http://{REMOTE_HOST}:{REMOTE_PORT}/receive_data"
 
     def __init__(self, window, window_title):
         self.window = window
         self.window.title(window_title)
+        
 
         self.q1 = Queue()
         self.q2 = Queue()
@@ -33,14 +40,16 @@ class AppCamera:
         self.pose = pm.mediapipe_pose()
         self.pt = self.pose.mp_holistic.Holistic()
         self.new_model = tf.keras.models.load_model('TrainedModel/ModeloBacano6.h5')
+        self.prev_action =  None
+        self.Visual = False
 
 
-        self.cap = cv2.VideoCapture(0)
+        
 
         self.canvas = customtkinter.CTkCanvas(window, width=800, height=600)
         self.canvas.pack()
 
-        self.label = customtkinter.CTkLabel(window, fg_color="transparent")
+        self.label = customtkinter.CTkLabel(window, fg_color="transparent", text='Acciones: ')
         self.label.pack()
 
         self.button = customtkinter.CTkButton(window, text="Start", command=self.camera)
@@ -48,6 +57,15 @@ class AppCamera:
 
         self.button_draw = customtkinter.CTkButton(window, text="Drawing on", command=self.drawing)
         self.button_draw.pack(pady=10)
+
+        self.button_visual = customtkinter.CTkButton(window, text="Visual on", command=self.VisualB)
+        self.button_visual.pack(pady=10)
+        
+        self.labeltext = customtkinter.CTkLabel(window, text="IP Camera link (http:/..): ")
+        self.labeltext.place(x=130, y=620)
+
+        self.tetxbox = customtkinter.CTkTextbox(window, height=10)
+        self.tetxbox.place(x=130, y=650)
 
         self.sequence = []
         self.sentence = []
@@ -63,12 +81,24 @@ class AppCamera:
         else:
             self.button_draw.configure(text="Drawing on")
 
+    def VisualB(self):
+        self.Visual = not(self.Visual)
+        if self.button_visual.cget("text") == "Visual on":
+            self.button_visual.configure(text="Visual off")
+        else:
+            self.button_visual.configure(text="Visual on")
+
     def camera(self):
         def processing():
-            cap = cv2.VideoCapture(0)
+            ip_camera =  self.tetxbox.get(index1=1.0, index2=customtkinter.END).strip()
+            if ip_camera == "0":
+                cap = cv2.VideoCapture(0)
+            else:
+                cap = cv2.VideoCapture(f"{ip_camera}")
             while cap.isOpened():
                 ret, frameVisual = cap.read()
-                frame = cv2.resize(frameVisual, (int(frameVisual.shape[1] / 4), int(frameVisual.shape[0] / 4)))
+                frameVisual = cv2.resize(frameVisual,(800,600))
+                frame = cv2.resize(frameVisual, (int(frameVisual.shape[1] / 8), int(frameVisual.shape[0] / 8)))
                 if not ret:
                     break
                 try:
@@ -85,7 +115,9 @@ class AppCamera:
                     res = self.new_model.predict(np.expand_dims(self.sequence, axis=0))[0]
                     if res[np.argmax(res)] > self.threshold:
                         action = self.actions[np.argmax(res)]
-                        #print(action)
+                        if action != self.prev_action:
+                            self.prev_action = action
+                            Thread(target=self.sendAction, args=(action,), daemon=True).start()
                         
                         if action == 'Alerta de Caida':
                             frameV = cv2.cvtColor(frameVisual, cv2.COLOR_BGR2RGB) 
@@ -95,6 +127,7 @@ class AppCamera:
                                 imageio.mimwrite('Caida2.gif', self.GIF, 'GIF')
                                 print('GIF guardado')
                                 self.GIF = []
+                                Thread(target=self.sendAlert, daemon=True).start()
                                 
                             else:
                                 pass
@@ -104,27 +137,42 @@ class AppCamera:
                         self.sentence = self.sentence[-1:]
                 text = ' '.join(self.sentence)
                 self.label.configure(text=text)
-                frame = cv2.putText(frameVisual, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frameVisual, cv2.COLOR_BGR2RGB)))
-                self.canvas.create_image(0, 0, image=photo, anchor=customtkinter.NW)
-
-
-        
-                    
-                    
-                
-
-        def sendAlert():
-            pass
-        
-
+                if self.Visual:
+                    frame = cv2.putText(frameVisual, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                    photo = ImageTk.PhotoImage(image=Image.fromarray(cv2.cvtColor(frameVisual, cv2.COLOR_BGR2RGB)))
+                    self.canvas.create_image(0, 0, image=photo, anchor=customtkinter.NW)
+                    self.canvas.image = photo  # Store a reference to the image to prevent it from being garbage collected
                 
         Thread(target=processing, daemon=True).start()
         #Thread(target=GenerateGIF,daemon=True).start()
+    def sendAlert(self):
+            try:
+                response = requests.post(self.RECEIVE_URL, files={'file': open('Caida2.gif', 'rb')})
+                if response.status_code == 200:
+                    print("Alerta enviada correctamente al servidor.")
+                else:
+                    print("Error al enviar la alerta al servidor. Código de estado:", response.status_code)
+            except Exception as e:
+                print("Error al enviar la alerta al servidor:", str(e))
+
+    def sendAction(self, action):
+        try:
+            fecha_a = time.time()
+            fecha_l = time.localtime(fecha_a)
+        
+            data = {'action': action, 'date': time.strftime("%Y-%m-%d %H:%M:%S",fecha_l)}
+            response = requests.post(self.RECEIVE_URL, json=data)
+            if response.status_code == 200:
+                print("Acción predicha enviada correctamente al servidor.")
+            else:
+                print("Error al enviar la acción predicha al servidor. Código de estado:", response.status_code)
+        except Exception as e:
+            print("Error al enviar la acción predicha al servidor:", str(e))
+
 
 if __name__ == "__main__":
     customtkinter.set_appearance_mode("dark")  # Modes: system (default), light, dark
     customtkinter.set_default_color_theme("blue")
     root = customtkinter.CTk()
-    root.geometry("960x720")
+    root.geometry("960x800")
     app = AppCamera(root, "Camera App")
